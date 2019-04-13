d322e700a9d5d850ce01a55f3f0e12e58b021f87
Paulo Varassin <pauloarthurvm@hotmail.com>
2019-04-13 14:13:27 -0300

    Updates

--- Server_Web_Multithread/src/server_web_multithread/Server_Web_Multithread.java
+++ Server_Web_Multithread/src/server_web_multithread/Server_Web_Multithread.java
@@ -5,6 +5,10 @@
  */
 package server_web_multithread;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 /**
  *
  * @author paulo
@@ -14,8 +18,24 @@ public class Server_Web_Multithread {
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
+	public static void main(String[] args) throws IOException {
+		
+		//	Set the port number
+		int port = 6789;
+		
+		//	Establish the listen socket
+		ServerSocket server = new ServerSocket(12345);
+		Socket client = server.accept();
+		
+		//	Process HTTP service requestes in an infinite loop.
+		while(true){
+			//	Listen for a TCP connection request
+		}
+		
+		
+		
+		
 	}
 	
 }
+
